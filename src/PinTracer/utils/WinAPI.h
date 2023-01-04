6f9a318a56777ae9b7a8ef347d15567bf158eac4
Marcos Bajo <marcossanchezbajo@gmail.com>
2023-01-04 20:36:53 +0100

    Updated winapi settings

--- src/PinTracer/utils/WinAPI.h
+++ src/PinTracer/utils/WinAPI.h
@@ -4,25 +4,9 @@
 #define WIN32_LEAN_AND_MEAN
 #define _WINDOWS_H_PATH_ C:/Program Files (x86)/Windows Kits/10/Include/10.0.22621.0/um
 
-/*namespace WINDOWS
+namespace WIN
 {
-
-#include <winapifamily.h>
-#include <windows.h>
-
-}*/
-
-namespace WINDOWS {
 #include <Windows.h>
-/*#include <winapifamily.h>
-#include <WinNT.h>
-#include <Winternl.h>
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#include <string.h>
-#include <tchar.h>
-#include <Psapi.h>
-#include <Strsafe.h>
-#define STATUS_SUCCESS 0*/
+
 }
 #endif
