a212c4a4c18704255a686a3dbad445b3e2ee216f
Marcos Bajo <marcossanchezbajo@gmail.com>
2023-01-05 02:08:47 +0100

    Fixed function pointers

--- src/PinTracer/taint/core/TaintSource.h
+++ src/PinTracer/taint/core/TaintSource.h
@@ -2,17 +2,25 @@
 #define _TAINT_SOURCE_H_
 
 #include "../utils/WinAPI.h"
+#include "../io/log.h"
 
 namespace TaintSource
 {
-
+	VOID wsockRecvEnter()
+	{
+		LOG("Not implemented");
+	}
+	VOID wsockRecvExit()
+	{
+		LOG("Not implemented");
+	}
 	struct wsock_recv_t
 	{
 		std::string dllName = "wsock32.dll";
 		std::string funcName = "recv";
 		int numArgs = 4;
-		AFUNPTR enterHandler = (AFUNPTR)wsock_recv_enter;
-		AFUNPTR exitHandler = (AFUNPTR)wsock_recv_exit;
+		AFUNPTR enterHandler = (AFUNPTR)wsockRecvEnter;
+		AFUNPTR exitHandler = (AFUNPTR)wsockRecvExit;
 
 		WIN::SOCKET s;
 		char* buf;
