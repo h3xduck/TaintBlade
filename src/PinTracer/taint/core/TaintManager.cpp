ee500f6c2a6f745d8659e24f7dcb8bc61355f4de
Marcos Bajo <marcossanchezbajo@gmail.com>
2023-01-09 14:29:29 +0100

    Added unregistration of tagsources in the function tag map

--- src/PinTracer/taint/core/TaintManager.cpp
+++ src/PinTracer/taint/core/TaintManager.cpp
@@ -17,12 +17,30 @@ void TaintManager::registerTaintSource(const std::string& dllName, const std::st
 
 
 	TaintSource taintSource(dllName, funcName, enterHandler, exitHandler);
-	const std::tr1::unordered_map<std::string, std::vector<TaintSource>>::const_iterator taintIt = this->taintFunctionMap.find(dllName);
+	const std::tr1::unordered_map<std::string, std::vector<TaintSource>>::iterator taintIt = this->taintFunctionMap.find(dllName);
 	if (taintIt == this->taintFunctionMap.end())
 	{
 		//DLLname is new
 		std::vector<TaintSource> taintVector;
 		taintVector.push_back(taintSource);
 		this->taintFunctionMap.insert(std::pair<std::string, std::vector<TaintSource>>(dllName, taintVector));
+	}
+	else
+	{
+		//DLLname already exists
+		taintIt->second.push_back(taintSource);
+	}
+}
+
+void TaintManager::unregisterTaintSource(const std::string& dllName, const std::string& funcName)
+{
+	const std::tr1::unordered_map<std::string, std::vector<TaintSource>>::const_iterator taintIt = this->taintFunctionMap.find(dllName);
+	if (taintIt == this->taintFunctionMap.end())
+	{
+		LOG_ERR("Tried to unregister inexistent taint source");
+	}
+	else
+	{
+		this->taintFunctionMap.erase(taintIt);
 	}
 }
