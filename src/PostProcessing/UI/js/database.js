8286b23c0fefefcea8e785fbee76369952b72ba8
Marcos Bajo <marcossanchezbajo@gmail.com>
2023-02-04 22:11:53 +0100

    Finished functionality of showing function arguments. Still need to add their actual guessed values once I dump them from the DB too.

--- src/PostProcessing/UI/js/database.js
+++ src/PostProcessing/UI/js/database.js
@@ -58,7 +58,7 @@ function executeGetFullTableResults(commands) {
 
         outputBoxElm.empty();
         for (var i = 0; i < results.length; i++) {
-            outputBoxElm.append(tableCreate(results[i].columns, results[i].values));
+            outputBoxElm.append(tableCreateGeneral(results[i].columns, results[i].values));
         }
     }
     worker.postMessage({ action: 'exec', sql: commands });
@@ -66,7 +66,7 @@ function executeGetFullTableResults(commands) {
     outputBoxElm.textContent = "Fetching results...";
 }
 
-// Run a command in the database, get a table with function calls and hidden args in dropdown
+// Run a command in the database, get a table with function calls
 // For function_calls table
 function executeFormatFunctionCalls(commands) {
     worker.onmessage = function (event) {
@@ -88,10 +88,33 @@ function executeFormatFunctionCalls(commands) {
     outputBoxElm.textContent = "Fetching results...";
 }
 
+// Run a command in the database, get args corresponding to a function call given an id
+// For the dropdown at the functions table
+function executeFormatFunctionArguments(commands, parentRow) {
+    worker.onmessage = function (event) {
+        console.log("Received message");
+        var results = event.data.results;
+        console.log(results);
+        if (!results) {
+            console.log("ERROR receiving worker results: " + event.data.error);
+            return;
+        }
+
+        for (var i = 0; i < results.length; i++) {
+            parentRow.after('<tr><td>');
+            parentRow.next().append(tableCreateGeneral(results[i].columns, results[i].values));
+            parentRow.next().append('</td></tr>');
+        }
+    }
+    worker.postMessage({ action: 'exec', sql: commands });
+    console.log("Posted message with commands:" + commands);
+    outputBoxElm.textContent = "Fetching results...";
+}
+
 ///////////////////// UI STUFF /////////////////////
 
 // Create an HTML table, all columns shown
-var tableCreate = function () {
+var tableCreateGeneral = function () {
     function valconcat(vals, tagName) {
         if (vals.length === 0) return '';
         var open = '<' + tagName + '>', close = '</' + tagName + '>';
@@ -117,16 +140,27 @@ var tableCreateFormatFunctionCalls = function () {
         return open + vals.join(close + open) + close;
     }
 
+    function valConcatWithPkey(vals, tagName) {
+        if (vals.length === 0) return '';
+        var open = '<' + tagName + '>', close = '</' + tagName + '>';
+        
+        //The one with appearance is hidden
+        var res =  '<' + tagName + ' class="pkey">' + vals[0] + close +
+            open + vals[1] + close + open + vals[2] + close + open + vals[3] + close +
+            open + vals[4] + close + open + vals[5] + close + open + vals[6] + close;
+        return res;
+    }
+
     return function (columns, values) {
         var tbl = document.createElement('table');
         tbl.className = "results-table";
         var html = '<thead>' +
             '<th>Action</th>' +
-            valconcat(columns, 'th') + '</thead>';
+            valConcatWithPkey(columns, 'th') + '</thead>';
         var rows = values.map(function (v) {
-            return '<td><button type="button" class="btn btn-success exploder">' +
+            return '<td><button type="button" class="btn btn-success exploder" onclick="exploderClick(this)">' +
                 ' <span class="fas fa-plus-square"></span>' +
-                '</button ></td >' + valconcat(v, 'td');
+                '</button ></td >' + valConcatWithPkey(v, 'td');
         });
         html += '<tbody>' + valconcat(rows, 'tr') +'</tbody>';
         tbl.innerHTML = html;
@@ -154,14 +188,35 @@ function showColorChanged(elem) {
     );
 }
 
-$(".exploder").click(function () {
-    $(this).toggleClass("btn-success btn-danger");
-    $(this).children("span").toggleClass("glyphicon-search glyphicon-zoom-out");
-    $(this).closest("tr").next("tr").toggleClass("hide");
-    if ($(this).closest("tr").next("tr").hasClass("hide")) {
-        $(this).closest("tr").next("tr").children("td").slideUp();
+function exploderClick(elem) {
+    //Check whether to show or hide arguments table
+    var icon = $(elem).children('span').eq(0);
+    if (icon.hasClass("fa-plus-square")) {
+        //Change the icon to hide element
+        var icon = $(elem).children('span').eq(0);
+        icon.removeClass("fa-plus-square");
+        icon.addClass("fa-minus-square");
+
+        //We generate an additional row where we will show the arguments of the function
+        var key = $(elem).parent().parent().find(".pkey").text();
+        var parentRow = $(elem).parent().parent();
+        executeFormatFunctionArguments(
+            "SELECT arg0, arg1, arg2, arg3, arg4, arg5 FROM function_calls " +
+            "WHERE appearance=" + key,
+            parentRow);
+
+    } else {
+        //Change the icon to hide element
+        var icon = $(elem).children('span').eq(0);
+        icon.removeClass("fa-minus-square");
+        icon.addClass("fa-plus-square");
+
+        //We remove the arguments table
+        var parentRow = $(elem).parent().parent();
+        parentRow.next().remove();
     }
-    else {
-        $(this).closest("tr").next("tr").children("td").slideDown(350);
-    }
-});
+
+    
+
+    
+}
